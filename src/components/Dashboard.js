00a14e47832ec6805e9cd7ff2c868d52342c18c6
Vanessa Yeung <vanessayeung@Vanessas-MacBook-Air.local>
2021-03-25 14:52:27 -0700

    import helpers

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,26 +5,33 @@ import axios from "axios";
 import Loading from "components/Loading.js";
 import Panel from "components/Panel.js";
 
-const data = [
+import {
+  getTotalInterviews,
+  getLeastPopularTimeSlot,
+  getMostPopularDay,
+  getInterviewsPerDay
+ } from "helpers/selectors";
+
+ const data = [
   {
     id: 1,
     label: "Total Interviews",
-    value: 6
+    getValue: getTotalInterviews
   },
   {
     id: 2,
     label: "Least Popular Time Slot",
-    value: "1pm"
+    getValue: getLeastPopularTimeSlot
   },
   {
     id: 3,
     label: "Most Popular Day",
-    value: "Wednesday"
+    getValue: getMostPopularDay
   },
   {
     id: 4,
     label: "Interviews Per Day",
-    value: "2.3"
+    getValue: getInterviewsPerDay
   }
 ];
 
